1edf997109afc034d1124ced862d1e15bcd72303
shark-ops <78763532+sarthak-ops@users.noreply.github.com>
2026-07-15 01:35:42 +0530

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.java
@@ -0,0 +1,23 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if(intervals.size() == 0) return true;
+        intervals.sort((a, b) -> a.start-b.start);
+        for(int i = 1; i < intervals.size(); i++){
+            if(intervals.get(i).start < intervals.get(i-1).end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
