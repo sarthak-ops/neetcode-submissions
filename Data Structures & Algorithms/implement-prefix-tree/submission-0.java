efc03c4678ddb143bacf316e5b3a33a3314fa415
shark-ops <78763532+sarthak-ops@users.noreply.github.com>
2026-07-18 23:08:17 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.java
@@ -0,0 +1,54 @@
+class PrefixTree {
+
+    class TrieNode{
+        TrieNode[] children;
+        boolean endOfWord;
+
+        public TrieNode(){
+            children = new TrieNode[26];
+            endOfWord = false;
+        }
+    }
+
+    TrieNode root;
+
+    public PrefixTree() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(node.children[c - 'a'] == null){
+                node.children[c-'a'] = new TrieNode();
+            }
+            node = node.children[c-'a'];
+        }
+        node.endOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(node.children[c-'a'] == null){
+                return false;
+            }
+            node = node.children[c-'a'];
+        }
+        return node.endOfWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for(int i = 0; i < prefix.length(); i++){
+            char c = prefix.charAt(i);
+            if(node.children[c-'a'] == null){
+                return false;
+            }
+            node = node.children[c-'a'];
+        }
+        return true;
+    }
+}
