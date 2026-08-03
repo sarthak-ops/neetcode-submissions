d2a2cfc425175e0d8e877e10a0cb2e2255522884
shark-ops <78763532+sarthak-ops@users.noreply.github.com>
2026-08-03 15:26:36 +0530

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++){
+            if((i ^ nums[i]) != 0){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
