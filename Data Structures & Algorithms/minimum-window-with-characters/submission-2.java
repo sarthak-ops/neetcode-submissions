1f9bbaec9fec0c407be010e2ff4363190a345b5f
shark-ops <78763532+sarthak-ops@users.noreply.github.com>
2026-08-19 00:08:54 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        String ans = "";
+        HashMap<Character, Integer> want = new HashMap<>();
+        for(int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            want.put(c, want.getOrDefault(c, 0) + 1);
+        }
+        int l = 0;
+        int r = 0;
+        int have = 0;
+        while(r < s.length()){
+            char c = s.charAt(r);
+            if(want.containsKey(c)){
+                if(want.get(c) > 0){
+                    have++;
+                }
+                want.put(c, want.get(c)-1);
+            }
+            r++;
+            while(have == t.length()){
+                String cur = s.substring(l, r);
+                if(ans.isEmpty() || cur.length() < ans.length()){
+                    ans = cur;
+                }
+                char c2 = s.charAt(l);
+                if(want.containsKey(c2)){
+                    want.put(c2, want.get(c2)+1);
+                    if(want.get(c2) > 0){
+                        have--;
+                    }
+                }
+                l++;
+            }
+        }
+        return ans;
+    }
+}
