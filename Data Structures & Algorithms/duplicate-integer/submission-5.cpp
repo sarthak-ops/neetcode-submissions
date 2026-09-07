b83a58b43004cb10122a6339d92c8d844b8d63bd
shark-ops <78763532+sarthak-ops@users.noreply.github.com>
2026-09-06 20:19:29 -0400

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        std::unordered_set<int> seen;
+        for(int n : nums){
+            if(seen.contains(n)){
+                return true;
+            }
+            seen.insert(n);
+        }
+        return false;
+    }
+};
